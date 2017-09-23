b98459032361fc73776d527451e296861d880b9e
imranlatur24 <imranlatur24@gmail.com>
2017-09-23 15:42:53 +0530

    Initial commit

---
+++ app/src/main/java/comandroiddeveloper/imranshaikh24/www/upreex/Constants.java
@@ -0,0 +1,9 @@
+package comandroiddeveloper.imranshaikh24.www.upreex;
+
+/**
+ * Created by root on 23/9/17.
+ */
+public class Constants {
+    public static final String UPLOAD_URL = "http://192.168.43.22/all/android/imgname/upload.php";
+    public static final String IMAGES_URL = "http://192.168.43.22/all/android/imgname/getImage.php";
+}
